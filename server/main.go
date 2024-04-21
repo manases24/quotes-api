86a49ace9fffd2cda194734fa25ef58e79648ab7
mnsh5 <hugo.orlando2050@gmail.com>
2024-04-21 20:26:58 -0300

    add new files

--- server/main.go
+++ server/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import "github.com/gin-gonic/gin"
+
+type User struct {
+	Name    string
+	Country string
+}
+
+func main() {
+	r := gin.Default()
+
+	user := &User{Name: "Jazz", Country: "Catland"}
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+			"user":    user,
+		})
+	})
+	r.Run(":2024")
+}
