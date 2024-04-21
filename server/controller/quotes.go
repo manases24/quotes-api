a480c84a8a06c7c931a54bc6eba37f016143bf77
mnsh5 <hugo.orlando2050@gmail.com>
2024-04-21 20:56:54 -0300

    add quotes controller

---
+++ server/controller/quotes.go
@@ -0,0 +1,18 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mnsh5/quotes/services"
+)
+
+type QuotesController struct {
+	quotesService services.QuotesService
+}
+
+func NewQuotesController(s services.QuotesService) *QuotesController {
+	return &QuotesController{quotesService: s}
+}
+
+func (q QuotesController) Create(c *gin.Context) error {
+	return q.quotesService.Create(c)
+}
